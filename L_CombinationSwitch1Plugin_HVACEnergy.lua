9d391bb8271b062c2a1057359cbd244711945a89
Deborah Pickett <github@dp.futzle.com>
2013-06-30 22:27:11 +0000

    HVAC mode conditions: Setpoints, Operating mode, Energy mode, Fan mode.

---
+++ L_CombinationSwitch1Plugin_HVACEnergy.lua
@@ -0,0 +1,80 @@
+module ("L_CombinationSwitch1Plugin_HVACEnergy", package.seeall)
+
+local MAIN
+
+local HVAC_SERVICE_ID = "urn:upnp-org:serviceId:HVAC_UserOperatingMode1"
+local HVAC_VARIABLE_MODE = "EnergyModeStatus"
+
+local INDEX_SWITCH_DEVICE_ID = "DeviceId"
+local INDEX_SWITCH_MODE = "MatchMode"
+
+function initialize(main)
+	MAIN = main
+	local supportedDevices = MAIN.getDevicesWithService(nil, HVAC_SERVICE_ID)
+	if (#supportedDevices == 0) then
+		return false
+	end
+	return true
+end
+
+function name()
+	return "HVAC Energy Mode"
+end
+
+function configureState(combinationDeviceId, index)
+	local supportedDevices = MAIN.getDevicesWithService(combinationDeviceId, HVAC_SERVICE_ID)
+	local jsSelectedDevice = MAIN.jsSetupState("selectedDevice", combinationDeviceId, MAIN.pluginId, index .. INDEX_SWITCH_DEVICE_ID, supportedDevices[1])
+	local jsMatchMode = MAIN.jsSetupState("matchMode", combinationDeviceId, MAIN.pluginId, index .. INDEX_SWITCH_MODE, [["Normal"]])
+
+	local result = "(function() {" ..
+		jsSelectedDevice ..
+		jsMatchMode ..
+		"return selectedDevice + '=' + matchMode; " ..
+		"})()"
+	return result;
+end
+
+function configureSelect(combinationDeviceId, index, state)
+	local selectedDevice, matchMode = state:match("(%d+)=(.+)")
+	local supportedDevices = MAIN.getDevicesWithService(combinationDeviceId, HVAC_SERVICE_ID)
+
+	local deviceSelectElement = MAIN.htmlSelectDevice(supportedDevices, selectedDevice, combinationDeviceId, MAIN.pluginId, index .. INDEX_SWITCH_DEVICE_ID)
+
+	local possibleMode = {
+		"EnergySavingsMode",
+		"Normal"
+	}
+	local result = deviceSelectElement .. " is in mode <select onchange='warnSave(); set_device_state(" .. combinationDeviceId .. ", \"" .. MAIN.pluginId .. "\", \"" .. index .. INDEX_SWITCH_MODE .. "\", $F(this), 0);'>"
+	for i = 1, #possibleMode do
+		result = result .. "<option value='" .. possibleMode[i] .. "'"
+		if (possibleMode[i] == matchMode) then
+			result = result .. " selected='selected'"
+		end
+		result = result .. ">" .. possibleMode[i] .. "</option>"
+	end
+	result = result .. "</select>"
+
+	return result
+end
+
+function register(combinationDeviceId, index)
+	MAIN.debug("Registering watch index " .. index)
+	local watchDeviceId = luup.variable_get(MAIN.pluginId, index .. INDEX_SWITCH_DEVICE_ID, combinationDeviceId)
+	if (watchDeviceId == nil) then return false end
+	local watchDeviceIdNum = tonumber(watchDeviceId)
+	if (watchDeviceIdNum >= 0 and luup.devices[watchDeviceIdNum]) then
+		local watchDeviceName = luup.devices[watchDeviceIdNum].description
+		local watchMode = luup.variable_get(MAIN.pluginId, index .. INDEX_SWITCH_MODE, DEVICE_ID)
+		MAIN.debug("Watching " .. watchDeviceName .. " [" .. watchDeviceId .. "] " ..
+			" HVAC Energy Mode: " .. watchMode)
+		luup.variable_watch("watch_callback", HVAC_SERVICE_ID, HVAC_VARIABLE_MODE, watchDeviceIdNum)
+	end
+	return true
+end
+
+function count(combinationDeviceId, index)
+	local watchDeviceId = luup.variable_get(MAIN.pluginId, index .. INDEX_SWITCH_DEVICE_ID, combinationDeviceId)
+	local watchDeviceIdNum = tonumber(watchDeviceId)
+	local watchMode = luup.variable_get(MAIN.pluginId, index .. INDEX_SWITCH_MODE, DEVICE_ID)
+	return luup.variable_get(HVAC_SERVICE_ID, HVAC_VARIABLE_MODE, watchDeviceIdNum) == watchMode and 1 or 0
+end
---
+++ L_CombinationSwitch1Plugin_HVACSetpointHeat.lua
@@ -0,0 +1,98 @@
+module ("L_CombinationSwitch1Plugin_HVACSetpointHeat", package.seeall)
+
+local MAIN
+
+local TEMPSETPOINT_SERVICE_ID = "urn:upnp-org:serviceId:TemperatureSetpoint1_Heat"
+local TEMPSETPOINT_VARIABLE_SETPOINT = "CurrentSetpoint"
+
+local INDEX_TEMPSETPOINT_DEVICE_ID = "DeviceId"
+local INDEX_TEMPSETPOINT_VALUE_LOW = "Low"
+local INDEX_TEMPSETPOINT_VALUE_HIGH = "High"
+
+function initialize(main)
+	MAIN = main
+	local supportedDevices = MAIN.getDevicesWithService(nil, TEMPSETPOINT_SERVICE_ID)
+	if (#supportedDevices == 0) then
+		return false
+	end
+	return true
+end
+
+function name()
+	return "Heat Setpoint"
+end
+
+function configureState(combinationDeviceId, index)
+	local supportedDevices = MAIN.getDevicesWithService(combinationDeviceId, TEMPSETPOINT_SERVICE_ID)
+	local jsSelectedDevice = MAIN.jsSetupState("selectedDevice", combinationDeviceId, MAIN.pluginId, index .. INDEX_TEMPSETPOINT_DEVICE_ID, supportedDevices[1])
+	local jsCurrentLow = MAIN.jsSetupState("currentLow", combinationDeviceId, MAIN.pluginId, index .. INDEX_TEMPSETPOINT_VALUE_LOW, [["1"]])
+	local jsCurrentHigh = MAIN.jsSetupState("currentHigh", combinationDeviceId, MAIN.pluginId, index .. INDEX_TEMPSETPOINT_VALUE_HIGH, [[""]])
+
+	local result = "(function() {" ..
+		jsSelectedDevice ..
+		jsCurrentLow ..
+		jsCurrentHigh ..
+		"return selectedDevice + '=' + currentLow + ',' + currentHigh; " ..
+		"})()"
+ 	return result;
+end
+
+function configureSelect(combinationDeviceId, index, state)
+	local selectedDevice, selectedLow, selectedHigh = state:match("(%d+)=(%-?[%d.]*),(%-?[%d.]*)")
+	local supportedDevices = MAIN.getDevicesWithService(combinationDeviceId, TEMPSETPOINT_SERVICE_ID)
+
+	local deviceSelectElement = MAIN.htmlSelectDevice(supportedDevices, selectedDevice, combinationDeviceId, MAIN.pluginId, index .. INDEX_TEMPSETPOINT_DEVICE_ID)
+
+	local result = MAIN.template([[
+		~deviceSelectElement~ is in range
+		<input type="text" size="4" value="~selectedLow~" onchange='warnSave(); set_device_state(~combinationDeviceId~, "~combinationServiceId~", "~index~~INDEX_TEMPSETPOINT_VALUE_LOW~", $F(this), 0);'>&deg; to
+		<input type="text" size="4" value="~selectedHigh~" onchange='warnSave(); set_device_state(~combinationDeviceId~, "~combinationServiceId~", "~index~~INDEX_TEMPSETPOINT_VALUE_HIGH~", $F(this), 0);'>&deg;
+		]], {
+			deviceSelectElement = deviceSelectElement,
+			combinationDeviceId = combinationDeviceId,
+			combinationServiceId = MAIN.pluginId,
+			index = index,
+			INDEX_TEMPSETPOINT_VALUE_LOW = INDEX_TEMPSETPOINT_VALUE_LOW,
+			INDEX_TEMPSETPOINT_VALUE_HIGH = INDEX_TEMPSETPOINT_VALUE_HIGH,
+			selectedLow = selectedLow,
+			selectedHigh = selectedHigh,
+		})
+
+	return result
+end
+
+function register(combinationDeviceId, index)
+	MAIN.debug("Registering watch index " .. index)
+	local watchDeviceId = luup.variable_get(MAIN.pluginId, index .. INDEX_TEMPSETPOINT_DEVICE_ID, combinationDeviceId)
+	if (watchDeviceId == nil) then return false end
+	local watchDeviceIdNum = tonumber(watchDeviceId)
+	if (watchDeviceIdNum >= 0 and luup.devices[watchDeviceIdNum]) then
+		local watchDeviceName = luup.devices[watchDeviceIdNum].description
+		local watchVariableValueLow = luup.variable_get(MAIN.pluginId, index .. INDEX_TEMPSETPOINT_VALUE_LOW, DEVICE_ID)
+		local watchVariableValueHigh = luup.variable_get(MAIN.pluginId, index .. INDEX_TEMPSETPOINT_VALUE_HIGH, DEVICE_ID)
+		MAIN.debug("Watching " .. watchDeviceName .. " [" .. watchDeviceId .. "] " ..
+			" Heating Setpoint: " .. watchVariableValueLow .. "-" .. watchVariableValueHigh)
+		luup.variable_watch("watch_callback", TEMPSETPOINT_SERVICE_ID, TEMPSETPOINT_VARIABLE_SETPOINT, watchDeviceIdNum)
+	end
+	return true
+end
+
+function count(combinationDeviceId, index)
+	local watchDeviceId = luup.variable_get(MAIN.pluginId, index .. INDEX_TEMPSETPOINT_DEVICE_ID, combinationDeviceId)
+	local watchDeviceIdNum = tonumber(watchDeviceId)
+	local currentValue = luup.variable_get(TEMPSETPOINT_SERVICE_ID, TEMPSETPOINT_VARIABLE_SETPOINT, watchDeviceIdNum)
+	local watchVariableValueLow = luup.variable_get(MAIN.pluginId, index .. INDEX_TEMPSETPOINT_VALUE_LOW, DEVICE_ID)
+	local inRange = 1
+	if (watchVariableValueLow ~= "") then
+		if (tonumber(currentValue) < tonumber(watchVariableValueLow)) then
+			inRange = 0
+		end
+	end
+	local watchVariableValueHigh = luup.variable_get(MAIN.pluginId, index .. INDEX_TEMPSETPOINT_VALUE_HIGH, DEVICE_ID)
+	if (watchVariableValueHigh ~= "") then
+		if (tonumber(currentValue) > tonumber(watchVariableValueHigh)) then
+			inRange = 0
+		end
+	end
+	return inRange
+end
